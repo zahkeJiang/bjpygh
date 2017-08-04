d359c088093e95bb501780f8f54eb1e1c114d9d6
huangfushan <243973775@qq.com>
2017-08-04 16:38:35 +0800

    huangfushan

--- src/main/webapp/js/recharge.js
+++ src/main/webapp/js/recharge.js
@@ -47,9 +47,9 @@ function userload(){
         }else{
             onBridgeReady();
         }*/
-        //微信支付
-/*function onBridgeReady(){
-     WeixinJSBridge.invoke(
+ //微信支付
+function onBridgeReady(){
+    WeixinJSBridge.invoke(
         'getBrandWCPayRequest', {
             "appId":"wx2421b1c4370ec43b",     //公众号名称，由商户传入     
             "timeStamp":"1395712654",         //时间戳，自1970年以来的秒数     
@@ -60,14 +60,18 @@ function userload(){
         },
         function(res){     
             if(res.err_msg == "get_brand_wcpay_request:ok" ) {
-                alert("支付成功");
-         }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-             else{
-                 alert("支付失败");
+                alert("微信支付成功");
+            }// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。              
+            else if(res.err_msg == "get_brand_wcpay_request:cancer"){
+                alert("用户取消支付");
+            }
+            else{
+                 alert("支付失败："+res.err_msg);
+            }
             }
          }
     ); 
-}*/
+}
 
 
 
