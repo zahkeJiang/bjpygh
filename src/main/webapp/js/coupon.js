321dce3ede10c6ae6c1e2c5556ab1b564032ac92
huangfushan <243973775@qq.com>
2017-08-04 17:03:42 +0800

    huangfushan

--- src/main/webapp/js/coupon.js
+++ src/main/webapp/js/coupon.js
@@ -23,12 +23,12 @@ $(function(){
 			}
 			var coupon = "<div class='coupon'>"+coupon_pic+"<div class='coupon_hint_text'><h2>优惠提示:</h2><p>30天有效期，从领取日开始计时</p><p>过期可使用会员积分激活继续使用</p></div><div class='use_coupon'></div></div>"
 			$(".container").html(coupon);
-			if (obj.status == "1" || obj.status == "2") {
+			if (obj.status == "1") {
     			$(".use_coupon").html("立即使用");
     			$(".use_coupon").click(function(){
     				window.location.href="index.html";
     			});
-    		}else if (obj.status == "3") {
+    		}else if (obj.status == "2") {
     			$(".use_coupon").html("立即激活并使用");
     			$(".use_coupon").click(function(){
     				$.post("activation.action",{},function(obj){
@@ -39,6 +39,8 @@ $(function(){
 						}
 					});
 				});	
+    		}else if (obj.status == "3") {
+    			$(".use_coupon").html("已使用");
     		}
 		}
     },'json');	
