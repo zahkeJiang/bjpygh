579860cc1c34e913e7f125b147f4f2b29a12649d
huangfushan <243973775@qq.com>
2017-08-08 14:46:17 +0800

    huangfushan

--- src/main/webapp/js/order_information.js
+++ src/main/webapp/js/order_information.js
@@ -8,14 +8,13 @@ $(function(){
 			$(".address").html(userorder.address);
 			$(".ds_training_mode").html(userorder.note);
 			$(".ds_name").html(userorder.dsname);
-			$(".ds_type").html(userorder.dstype+"&frasl;"+userorder.models+"&frasl;"+userorder.traintime);
+			$(".ds_type").html(userorder.dstype+"&nbsp;&frasl;&nbsp;"+userorder.models+"&nbsp;&frasl;&nbsp"+userorder.traintime);
 			$(".description").html(userorder.description);
 			$(".order_number").html(userorder.ordernumber);
 			$(".order_time").html(userorder.paytime);
 			$(".ds_pay").html("¥"+userorder.orderprice+".00");
 			$(".ds_price").html("¥"+(userorder.orderprice+obj.price)+".00");
 			$(".ds_coupon").html("¥"+obj.price+".00");
-
 			if (userorder.orderstatus=="1"||userorder.orderstatus=="2") {
 				var refund = "<div class='refund'>取消订单</div>";
 				$(".footer").html(refund);
@@ -23,30 +22,48 @@ $(function(){
 					window.location.href="ds_refund.html?ordernumber="+userorder.ordernumber;
 				});
 				if (userorder.orderstatus=="2") {
-					$(".submit_order p").css({"color":"#67c0ee"});
 					$(".material_audit p").css({"color":"#67c0ee"});
+					$(".material_audit h2").css({"color":"#67c0ee"});
 					$("#cartoon2").attr('src',"images/alipay.png");
+					$(".audit_bg").css({"background-color":"#67c0ee"});
+					
 					$(".hint").html("您的材料正在接受审核，24小时之内审核完成。");
 				}else{
-					$(".submit_order p").css({"color":"#67c0ee"});
-					// var cartton = "<img src='images/alipay.png' height="30px">"
-					$("#cartoon").attr('src',"images/alipay.png");
 					$(".hint").html("您的订单已经提交成功。请准备好所需材料，24小时之内将会有工作人员与您联系上门提取，请保持电话畅通。");
 				}
 			}else if (userorder.orderstatus=="3"||userorder.orderstatus=="4") {
 				var result = "<div class='result'><p>您已报名完成。</p><p>欢迎您再次使用。</p><p>北京漂洋过海，一切因你而在！</p></div>";
 				$(".footer").html(result);
 				if (userorder.orderstatus=="4") {
-					$(".submit_order p").css({"color":"#67c0ee"});
 					$(".material_audit p").css({"color":"#67c0ee"});
+					$(".material_audit h2").css({"color":"#67c0ee"});
+					$("#cartoon2").attr('src',"images/alipay.png");
+					$(".audit_bg").css({"background-color":"#67c0ee"});
+
 					$(".finish_order p").css({"color":"#67c0ee"});
+					$(".finish_order h2").css({"color":"#67c0ee"});
+					$("#cartoon3").attr('src',"images/alipay.png");
+					$(".finish_bg").css({"background-color":"#67c0ee"});
+
 					$(".material_return p").css({"color":"#67c0ee"});
-					$(".hint").html("您已完成报名，材料正在返还。");
-				}else{
-					$(".submit_order p").css({"color":"#67c0ee"});
-					$(".material_audit p").css({"color":"#67c0ee"});
-					$(".finish_order p").css({"color":"#67c0ee"});
+					$(".material_return h2").css({"color":"#67c0ee"});
+					$("#cartoon4").attr('src',"images/alipay.png");
+					$(".return_bg").css({"background-color":"#67c0ee"});
+
+					
 					$(".hint").html("您已报名完成，北京漂洋过海，一切因你而在！欢迎您再次使用！");
+				}else{
+					$(".material_audit p").css({"color":"#67c0ee"});
+					$(".material_audit h2").css({"color":"#67c0ee"});
+					$("#cartoon2").attr('src',"images/alipay.png");
+					$(".audit_bg").css({"background-color":"#67c0ee"});
+
+					$(".finish_order p").css({"color":"#67c0ee"});
+					$(".finish_order h2").css({"color":"#67c0ee"});
+					$("#cartoon3").attr('src',"images/alipay.png");
+					$(".finish_bg").css({"background-color":"#67c0ee"});
+					$(".hint").html("您已完成报名，材料正在返还。");
+					
 				}
 			}
 
