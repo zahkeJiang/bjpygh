403adb4fe752fb8e4cabfd8abd1cd1e8b94f52a2
huangfushan <243973775@qq.com>
2017-08-08 15:23:58 +0800

    huangfushan

--- src/main/webapp/js/order_information.js
+++ src/main/webapp/js/order_information.js
@@ -15,29 +15,33 @@ $(function(){
 			$(".ds_pay").html("¥&nbsp;"+userorder.orderprice+".00");
 			$(".ds_price").html("¥&nbsp;"+(userorder.orderprice+obj.price)+".00");
 			$(".ds_coupon").html("¥&nbsp;"+obj.price+".00");
-			var submittime = userorder.paytime;
-			var submit_data = submittime.split(' ')[0];
-			var submit_time = submittime.split(" ")[1];
+			var submitTime = userorder.paytime;
+			var submit_data = submitTime.split(' ')[0];
+			var submit_time = submitTime.split(" ")[1];
+			alert("1");
 			$(".submit_data").html(submit_data);
 			$(".submit_time").html(submit_time);
 
 			var audittime = userorder.submittime;
 			var audit_data = audittime.split(' ')[0];
 			var audit_time = audittime.split(" ")[1];
-			$(".audit_data").html(submit_data);
-			$(".audit_time").html(submit_time);
+			alert("2");
+			$(".audit_data").html(audit_data);
+			$(".audit_time").html(audit_time);
 
 			var finishtime = userorder.signtime;
 			var finish_data = finishtime.split(' ')[0];
 			var finish_time = finishtime.split(" ")[1];
-			$(".finish_data").html(submit_data);
-			$(".finish_time").html(submit_time);
+			alert("3");
+			$(".finish_data").html(finish_data);
+			$(".finish_time").html(finish_time);
 
 			var returntime = userorder.gettime;
 			var return_data = returntime.split(' ')[0];
 			var return_time = returntime.split(" ")[1];
-			$(".return_data").html(submit_data);
-			$(".return_time").html(submit_time);
+			alert("4");
+			$(".return_data").html(return_data);
+			$(".return_time").html(return_time);
 			if (userorder.orderstatus=="1"||userorder.orderstatus=="2") {
 				var refund = "<div class='refund'>取消订单</div>";
 				$(".footer").html(refund);
