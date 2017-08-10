0fd72b2189eb23835a576d739ffc94996055995a
zahkeJiang <1345802063@qq.com>
2017-08-10 09:33:40 +0800

    add myconfig --Jiang

---
+++ src/main/java/com/wxgzpt/bjpygh/config/MyConfig.java
@@ -0,0 +1,45 @@
+package com.wxgzpt.bjpygh.config;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class MyConfig {
+
+    private byte[] certData;
+
+    public MyConfig() throws Exception {
+        String certPath = "/path/to/apiclient_cert.p12";
+        File file = new File(certPath);
+        InputStream certStream = new FileInputStream(file);
+        this.certData = new byte[(int) file.length()];
+        certStream.read(this.certData);
+        certStream.close();
+    }
+
+    public String getAppID() {
+        return "wx8888888888888888";
+    }
+
+    public String getMchID() {
+        return "12888888";
+    }
+
+    public String getKey() {
+        return "88888888888888888888888888888888";
+    }
+
+    public InputStream getCertStream() {
+        ByteArrayInputStream certBis = new ByteArrayInputStream(this.certData);
+        return certBis;
+    }
+
+    public int getHttpConnectTimeoutMs() {
+        return 8000;
+    }
+
+    public int getHttpReadTimeoutMs() {
+        return 10000;
+    }
+}
