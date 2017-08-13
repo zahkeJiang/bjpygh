514fe013c149788b0a853b4ce5cc34d57a45f6f1
huangfushan <243973775@qq.com>
2017-08-13 10:52:20 +0800

    huangfushan

--- src/main/webapp/js/integral.js
+++ src/main/webapp/js/integral.js
@@ -1,10 +1,15 @@
 $(function(){
+	$.post("personal.action",{},function(obj){
+		if (obj.status=="1") {
+			var integral = obj.data;
+			$(".my_integral").html(integral.memberpoints);
+		}
+	},"json");
 	$(".recharge").click(function(){
 		window.location.href="recharge.html";
-
 	});
 	$(".integral_type").click(function(){
 		alert("正在建设中..");
-	})
+	});
 });
 		
