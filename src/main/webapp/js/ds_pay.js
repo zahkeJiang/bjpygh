df7ba19dadb9336a49afec1bf26f1a01f4c227d3
huangfushan <243973775@qq.com>
2017-08-15 21:10:54 +0800

    huangfushan

--- src/main/webapp/js/ds_pay.js
+++ src/main/webapp/js/ds_pay.js
@@ -11,14 +11,12 @@ $(function(){
     //发送请求判断支付完成与否
     $.post("queryOrder.action",{},function(obj){
         if(obj.status=="1"){              
-            alert("支付成功");
             var order_number = obj.out_trade_no;
             var ds_price = obj.price;
             $(".order_number").html(order_number);
             $(".ds_price").html(ds_price);
             $(".pay_success").show();
         }else if (obj.status=="0") {
-            alert("支付失败");
             $(".pay_fail").show();
         }
     },"json");
